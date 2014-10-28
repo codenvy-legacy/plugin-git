c8fea720adc752a9d84eb754c5e805eeac36fb2c
Roman Nikitenko <rnikitenko@codenvy.com>
2014-10-28 11:22:12 +0200

    IDEX-1456. Fixed: Wrong behaviour of Git -> Reset Index.

--- codenvy-ext-git/src/main/java/com/codenvy/ide/ext/git/server/nativegit/commands/ResetCommand.java
+++ codenvy-ext-git/src/main/java/com/codenvy/ide/ext/git/server/nativegit/commands/ResetCommand.java
@@ -13,6 +13,7 @@ package com.codenvy.ide.ext.git.server.nativegit.commands;
 import com.codenvy.ide.ext.git.server.GitException;
 
 import java.io.File;
+import java.util.List;
 
 /**
  * Reset repository to specifically state
@@ -21,8 +22,9 @@ import java.io.File;
  */
 public class ResetCommand extends GitCommand<Void> {
 
-    private String commit;
-    private String mode;
+    private String       commit;
+    private String       mode;
+    private List<String> filePattern;
 
     public ResetCommand(File repository) {
         super(repository);
@@ -35,10 +37,22 @@ public class ResetCommand extends GitCommand<Void> {
             throw new GitException("Reset mode wasn't set.");
         }
         reset();
-        commandLine.add("reset", mode);
+        commandLine.add("reset");
+
         if (commit != null) {
             commandLine.add(commit);
         }
+
+        if (filePattern != null && !filePattern.isEmpty()) {
+            commandLine.add("--");
+            for (String file : filePattern) {
+                commandLine.add(file);
+            }
+        } else {
+            //Add mode don't makes sense for reset with paths(--mixed with paths is deprecated).
+            commandLine.add(mode);
+        }
+
         start();
         return null;
     }
@@ -60,6 +74,17 @@ public class ResetCommand extends GitCommand<Void> {
      */
     public ResetCommand setMode(String mode) {
         this.mode = mode;
+        return this;
+    }
+
+    /**
+     * Set up file pattern for reset command.
+     *
+     * @param pattern file pattern for reset command.
+     * @return ResetCommand with established pattern
+     */
+    public ResetCommand setFilePattern(List<String> pattern) {
+        this.filePattern = pattern;
         return this;
     }
 }

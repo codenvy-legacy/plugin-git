ec52c0e0e5540fdc4f9ba4b570ed321adb380f4e
Mihail Kuznetsov <mkuznyetsov@codenvy.com>
2014-10-29 15:02:36 +0200

    IDEX-377 add SafeHtml to avoid XSS vulnerabilities

--- codenvy-ext-git/src/main/java/com/codenvy/ide/ext/git/client/add/AddToIndexViewImpl.java
+++ codenvy-ext-git/src/main/java/com/codenvy/ide/ext/git/client/add/AddToIndexViewImpl.java
@@ -16,6 +16,7 @@ import com.codenvy.ide.ui.window.Window;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Button;
@@ -90,7 +91,7 @@ public class AddToIndexViewImpl extends Window implements AddToIndexView {
     /** {@inheritDoc} */
     @Override
     public void setMessage(@Nonnull String message) {
-        this.message.getElement().setInnerHTML(message);
+        this.message.getElement().setInnerHTML(SafeHtmlUtils.fromString(message).asString());
     }
 
     /** {@inheritDoc} */

cc4c056c3ac5431681794b4edf6d0ffdab0e63b5
Alexander Reshetnyak <areshetnyak@codenvy.com>
2014-12-03 10:28:55 +0200

    DASHB-586: Move AnalyticsEventLogger to client package.

--- codenvy-ext-git/src/main/java/com/codenvy/ide/ext/git/client/action/ResetFilesAction.java
+++ codenvy-ext-git/src/main/java/com/codenvy/ide/ext/git/client/action/ResetFilesAction.java
@@ -10,7 +10,7 @@
  *******************************************************************************/
 package com.codenvy.ide.ext.git.client.action;
 
-import com.codenvy.api.analytics.logger.AnalyticsEventLogger;
+import com.codenvy.api.analytics.client.logger.AnalyticsEventLogger;
 import com.codenvy.ide.api.action.ActionEvent;
 import com.codenvy.ide.api.app.AppContext;
 import com.codenvy.ide.api.selection.SelectionAgent;

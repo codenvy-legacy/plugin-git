c60e2e1dc6f07acb67b9e6b7400511d6f9ec15cc
Vitaliy Guliy <vguliy@codenvy.com>
2015-01-15 15:52:33 +0200

    IDEUI-430 Restored broken key navigation

--- codenvy-ext-git/src/main/java/com/codenvy/ide/ext/git/client/merge/ReferenceTreeNodeDataAdapter.java
+++ codenvy-ext-git/src/main/java/com/codenvy/ide/ext/git/client/merge/ReferenceTreeNodeDataAdapter.java
@@ -90,4 +90,10 @@ public class ReferenceTreeNodeDataAdapter implements NodeDataAdapter<Reference> 
         return null;
     }
 
+    /** {@inheritDoc} */
+    @Override
+    public Reference getNodeByPath(Reference root, Array<String> relativeNodePath) {
+        return null;
+    }
+
 }
